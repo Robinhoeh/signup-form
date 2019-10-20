622ceaa66ba4d497622b7b1b9a50f85f19099bb8
Robin <drumsrobin@gmail.com>
2019-10-20 12:05:19 -0400

    checking for field types

--- js/validation.js
+++ js/validation.js
@@ -5,11 +5,19 @@ elementsWithValidate.forEach((element) => {
   element.setAttribute('novalidate', true)
 })
 
-// Listen to events on blue
+// Validate the field
+const hasError = ((field) => {
+// Don't validate submits, buttons, file and reset inputs, and disabled fields
+  if(field.disabled || field.type === 'file' || field.type ===
+  'reset' || field.type === 'button' || field.type === 'submit') return
 
+  const validity = field.validity
+})
+
+// Listen to events on blur
 document.addEventListener('blur', (e) => {
   if(!e.target.form.classList.contains('validate')) return
 
-  let err = e.target.validity
-  console.log(err)
+  let error = hasError(e.target)
+
 }, true)
